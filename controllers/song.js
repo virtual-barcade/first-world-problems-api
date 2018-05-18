38f6cbb1684823c6f08c3b4852f4eefd2a5864df
Kevin Amirdjanian <kevin.amirdjanian@gmail.com>
2018-05-17 22:35:17 -0700

    [refactor] DRY up helper files

--- controllers/song.js
+++ controllers/song.js
@@ -1,5 +1,5 @@
 const spotify = require('../helpers/spotify.js');
-const giphy = require('../helpers/giphy.js');
+/* const giphy = require('../helpers/giphy.js'); */
 const songQueryGenerator = require('../helpers/song-query-generator.js');
 
 module.exports.searchSongs = async (req, res) => {
@@ -7,7 +7,7 @@ module.exports.searchSongs = async (req, res) => {
   try {
     const token = await spotify.fetchAccessToken();
     const song = await spotify.fetchSong(q, token.access_token);
-    const gif = await giphy.fetchGif('pokemon');
+    /* const gif = await giphy.fetchGif('pokemon'); */
     res.json(song.tracks.items[0]);
   } catch (error) {
     res.json(error);
