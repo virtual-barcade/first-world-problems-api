8317e658d3870d817e2322c3a314c01c8ed25885
Kevin Amirdjanian <kevin.amirdjanian@gmail.com>
2018-05-20 19:50:32 -0700

    [fix] remove reference to client code in app.js

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 require('dotenv').config();
 const express = require('express');
-const path = require('path');
 const bodyParser = require('body-parser');
 const morgan = require('morgan');
 
@@ -13,10 +12,5 @@ app.use(morgan('dev'));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use('/api', routes);
-
-app.use(express.static(path.join(__dirname, 'dist')));
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'dist/index.html'));
-});
 
 app.listen(PORT, () => console.log(`Listening on port ${PORT}...`));
