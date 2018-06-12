c11f6d8555c1cf8ce7600b349e0da89ed38cb0b0
Kevin Amirdjanian <kevin.amirdjanian@gmail.com>
2018-06-12 14:54:13 -0700

    [refactor] update song-query-generator logic

--- helpers/keyword-library.js
+++ helpers/keyword-library.js
@@ -1,23 +1,13 @@
 const keywords = {
   pizza: 'food',
-  girlfriend: 'partner',
-  boyfriend: 'partner',
-  partner: 'partner',
-  ipad: 'electronics',
-  laptop: 'electronics',
   enough: 'food',
   chips: 'food',
   food: 'food',
-  smartphone: 'electronics',
   hungry: 'food',
-  fridge: 'electronics',
-  refigerator: 'electronics',
   dip: 'food',
   restaurants: 'food',
-  ex: 'partner',
-  'ex-boyfriend': 'partner',
-  'ex-girlfriend': 'partner',
-  'ex-partner': 'partner',
+  wallet: 'money',
+  cash: 'money',
 };
 
 module.exports = keywords;
